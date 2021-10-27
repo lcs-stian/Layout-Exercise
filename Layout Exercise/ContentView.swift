15ada5b7af5598a46a3901b9e29cda115eddf16b
Suzanne <STian@lcs.on.ca>
2021-10-27 11:53:58 -0400

    Initial Commit

---
+++ Layout Exercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Layout Exercise
+//
+//  Created by Suzanne Tian on 2021-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Layout Exercise/Layout_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_ExerciseApp.swift
+//  Layout Exercise
+//
+//  Created by Suzanne Tian on 2021-10-27.
+//
+
+import SwiftUI
+
+@main
+struct Layout_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
